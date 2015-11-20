ad5f0d0e4f1d1abc5309ef8051597bda74a93e1f
wangzhizhou <824219521@qq.com>
2015-11-20 19:27:23 +0800

    添加了参数提示

--- Outline Extract/main.cpp
+++ Outline Extract/main.cpp
@@ -5,104 +5,139 @@
 #include "imagesStitcher.h"
 #include "viewer.h"
 
+bool isDispCorrectRet = false;
+bool saveResult = false;
+
+static void help()
+{
+	printf("This is a navigator based on fisheye images.\n"
+		"Usage: navigator\n"
+		"     [-d]          # display the corrected fisheye images.\n"
+		"     [-s]          # save the result of image process into files.\n"
+		"     [-h]          # show the help information.\n"
+		"\n");
+
+}
+void paserParams(int argc, char**argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1;i < argc;i++)
+		{
+			if (strcmp(argv[i], "-d")==0)
+				isDispCorrectRet = true;
+			else if (strcmp(argv[i], "-s")==0)
+				saveResult = true;
+			else if (strcmp(argv[i], "-h") == 0)
+			{
+				help();
+				exit(0);
+			}
+		}
+	}
+}
+
 //主程度入口点
 int main(int argc, char** argv)
-{	
+{
+	paserParams(argc, argv);
+
 	correctParameters params;
-	bool isDispCorrectRet = false;
 	corrector adjuster;
 	vector<Mat> outputs;
-
 	imagesStitcher stitcher;
-
-	bool saveResult = true;
 
 	//读图片到内存中
 	if (tools::readImage())
 	{
 		vector<Mat>& inputs = tools::GetImages();
 
-			Mat source_image = inputs[0];
+		Mat source_image = inputs[0];
 
-			if (findCircleParameter::init(source_image))
-			{
-				findCircleParameter::findCircle();				 
+		if (findCircleParameter::init(source_image))
+		{
+			findCircleParameter::findCircle();
 
 #pragma region 校正参数设定区
-				params.imgOrg = source_image;
-				findCircleParameter::getCircleParatemer(params.center, params.radius);
-				params.w_longtitude = PI / 2;
-				params.w_latitude = PI / 2;
-				params.distMap = LATITUDE_LONGTITUDE;
-				params.theta_left = 0;
-				params.phi_up = 0;
-				params.camerFieldAngle = findCircleParameter::FOV;
-				params.camProjMode = EQUIDISTANCE;
-				params.typeOfCorrect = Reverse;
+			params.imgOrg = source_image;
+			findCircleParameter::getCircleParatemer(params.center, params.radius);
+			params.w_longtitude = PI / 2;
+			params.w_latitude = PI / 2;
+			params.distMap = LATITUDE_LONGTITUDE;
+			params.theta_left = 0;
+			params.phi_up = 0;
+			params.camerFieldAngle = findCircleParameter::FOV;
+			params.camProjMode = EQUIDISTANCE;
+			params.typeOfCorrect = Reverse;
 #pragma endregion								
 
-				cout <<endl			
-					<< "Correct Image(s) with the same circular region params: " << endl
-					<< "radius = " << params.radius << ", center = " << params.center << endl << endl;
+			cout << endl
+				<< "Correct Image(s) with the same circular region params: " << endl
+				<< "radius = " << params.radius << ", center = " << params.center << endl << endl;
 
 #pragma region 图像校正区
-				corrector::correctMethod method = corrector::correctMethod::PERSPECTIVE_LONG_LAT_MAP_CAMERA_LEN_MODEL_REVERSE_W_HALF_PI;
+			corrector::correctMethod method = corrector::correctMethod::PERSPECTIVE_LONG_LAT_MAP_CAMERA_LEN_MODEL_REVERSE_W_HALF_PI;
+
+			outputs.push_back(
+				adjuster.correctImage(params, method,
+					isDispCorrectRet)
+				);
+
+			for (int i = 1;i < inputs.size();i++)
+			{
+				source_image = inputs[i];
+				params.imgOrg = source_image;
 
 				outputs.push_back(
-					adjuster.correctImage(params, method,
-						isDispCorrectRet)
+					adjuster.correctImage(params, method, isDispCorrectRet)
 					);
-
-				for (int i = 1;i < inputs.size();i++)
-				{
-					source_image = inputs[i];
-					params.imgOrg = source_image;
-
-					outputs.push_back(
-						adjuster.correctImage(params, method,isDispCorrectRet)
-						);
-				}
 			}
-		}	
+		}
+	}
 #pragma endregion
-	cout << endl
-		<<"Start Stitch images" << endl;
+
 #pragma region 图像接接部分
+	vector<Mat> images1, images2;
+	string filenamePath;
+	char suffix[MAX_PATH];
+	for (int i = 0;i < outputs.size();i++)
+	{
+		if (saveResult)
+		{
+			sprintf_s(suffix, "corrected%d.jpg", i);
+			string tmp(suffix);
+			filenamePath = tmp;
+			imwrite(filenamePath, outputs[i]);
+		}
+		if (i < outputs.size() - 1)
+			images1.push_back(outputs[i]);
+		else
+			images2.push_back(outputs[i]);
+	}
 
 	if (outputs.size() > 1)
 	{
-		vector<Mat> images1, images2;
-		string filenamePath;
-		char suffix[MAX_PATH];
-		for (int i = 0;i < outputs.size();i++)
-		{
-			if (saveResult)
-			{
-				sprintf_s(suffix, "_corrected%d.jpg", i);
-				string tmp(suffix);
-				filenamePath = tmp;
-				imwrite(filenamePath, outputs[i]);
-			}
+		cout << endl << "Start Stitch images" << endl;
 
-			if (i < outputs.size() - 1)
-				images1.push_back(outputs[i]);
-			else
-				images2.push_back(outputs[i]);
-		}
 		Mat tmpRet = stitcher.stitchImages(images1);
 		images2.insert(images2.begin(), tmpRet.clone());
 		stitcher.stitchImages(images2);
 		stitcher.showPanorama();
-	}
-#pragma endregion
-	cout << "Stitch Images completes!" << endl;
 
-#pragma region 浏览全景图像
-	Mat panoramaImage = stitcher.getPanorama();
-	if (panoramaImage.empty()) return 0;
-	imwrite("panorama.jpg", panoramaImage);
-	viewer panoViewer(panoramaImage);
-	panoViewer.showWindow();
+		cout << "Stitch Images completes!" << endl;
 #pragma endregion
+#pragma region 浏览全景图像
+
+		Mat panoramaImage = stitcher.getPanorama();
+
+		if (!panoramaImage.empty())
+		{
+			if (saveResult)	imwrite("panorama.jpg", panoramaImage);
+
+			viewer panoViewer(panoramaImage);
+			panoViewer.showWindow();
+		}
+#pragma endregion
+	}
 	return 0;
 }
