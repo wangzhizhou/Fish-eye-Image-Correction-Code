f70cd932c60f63d38e2a2596e75d7044fe129932
wangzhizhou <824219521@qq.com>
2024-08-28 01:16:04 +0800

    add OrzCalibration app

---
+++ opencv/make_xcframework.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#-*- coding: utf-8 -*-
+
+script_dir=$(dirname "$0")
+python3 ${script_dir}/main/platforms/apple/build_xcframework.py \
+--macos_archs arm64 \
+--iphoneos_archs arm64 \
+--iphonesimulator_archs arm64 \
+--catalyst_archs arm64 \
+--build_only_specified_archs \
+--out ${script_dir}/build_xcframework
