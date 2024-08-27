e4e6a993513a54f98f07cb601a7dc97a419231d7
wangzhizhou <824219521@qq.com>
2024-08-28 01:45:29 +0800

    build mac opencv

--- opencv/make_xcframework.sh
+++ opencv/make_xcframework.sh
@@ -3,9 +3,10 @@
 
 script_dir=$(dirname "$0")
 python3 ${script_dir}/main/platforms/apple/build_xcframework.py \
---macos_archs arm64 \
---iphoneos_archs arm64 \
---iphonesimulator_archs arm64 \
---catalyst_archs arm64 \
+--macos_archs arm64,arm64e \
 --build_only_specified_archs \
 --out ${script_dir}/build_xcframework
+
+# --iphoneos_archs arm64 \
+# --iphonesimulator_archs arm64 \
+# --catalyst_archs arm64 \
